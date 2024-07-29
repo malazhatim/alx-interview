6ab2ec20d4698228a94f6c96d05f9bbb760db3fc
malazhatim <malaz.hatim@gmail.com>
2024-07-29 18:52:55 +0000

    0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+
+def canUnlockAll(boxes):
+    a = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= a or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return a == len(seen_boxes)
